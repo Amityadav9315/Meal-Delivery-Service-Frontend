c0aeb9e21713b56a0053b1d2639e5f82e3b0033d
Amit Yadav <amit931552@gmail.com>
2025-06-22 22:09:56 +0530

    vfwbklbklvrf

--- src/component/Cart/Cart.jsx
+++ src/component/Cart/Cart.jsx
@@ -1,12 +1,13 @@
-import { Divider } from '@mui/material'
+import Divider from '@mui/material/Divider';
 import React from 'react'
+import { CartItem } from './CartItem'
 
 const Cart = () => {
   return (
     <div>
         <main className='lg:flex justify-between'>
             <section className='lg:w-[30%] space-y-6 lg:main-h-screen pt-10'>
-
+        <CartItem/>
             </section>
             <Divider/>
         </main>
