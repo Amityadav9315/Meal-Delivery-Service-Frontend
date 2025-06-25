fd800fa918b584960dd8acf1c54ac9c3c47b74dd
Amit Yadav <amit931552@gmail.com>
2025-06-25 18:53:21 +0530

    sfvnkfd

--- src/component/Profile/ProfileNavigation.jsx
+++ src/component/Profile/ProfileNavigation.jsx
@@ -2,6 +2,10 @@ import React from 'react'
 import ShoppingBagIcon from '@mui/icons-material/ShoppingBag';
 import FavoriteIcon from '@mui/icons-material/Favorite';
 import HomeIcon from '@mui/icons-material/Home';
+import AccountBalanceWalletIcon from '@mui/icons-material/AccountBalanceWallet';
+import NotificationsActiveIcon from '@mui/icons-material/NotificationsActive';
+import EventIcon from '@mui/icons-material/Event';
+import LogoutIcon from '@mui/icons-material/Logout';
 
 
 const menu=[
