9ff5c642c5f63133f62a868b80195f620ffc90fc
Amit Yadav <amit931552@gmail.com>
2025-06-25 18:50:16 +0530

    fjkgbfd

---
+++ src/component/Profile/ProfileNavigation.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import ShoppingBagIcon from '@mui/icons-material/ShoppingBag';
+import FavoriteIcon from '@mui/icons-material/Favorite';
+import HomeIcon from '@mui/icons-material/Home';
+
+
+const menu=[
+    {title:"Orders",icon<ShoppingBagTcon/>}
+]
+const ProfileNavigation = () => {
+  return (
+    <div>
+
+    </div>
+  )
+}
+
+export default ProfileNavigation
