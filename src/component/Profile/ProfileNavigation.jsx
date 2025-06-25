348b1960d07e9ce937df37f21a1d40d922c6086e
Amit Yadav <amit931552@gmail.com>
2025-06-25 19:02:08 +0530

    sfvnkfd

--- src/component/Profile/ProfileNavigation.jsx
+++ src/component/Profile/ProfileNavigation.jsx
@@ -6,10 +6,17 @@ import AccountBalanceWalletIcon from '@mui/icons-material/AccountBalanceWallet';
 import NotificationsActiveIcon from '@mui/icons-material/NotificationsActive';
 import EventIcon from '@mui/icons-material/Event';
 import LogoutIcon from '@mui/icons-material/Logout';
-
-
+import AccountBalanceWalletIcon from '@mui/icons-material/AccountBalanceWallet';
+import AddReactionIcon from '@mui/icons-material/AddReaction';
 const menu=[
-    {title:"Orders",icon<ShoppingBagTcon/>}
+    {title:"Orders",icon:<ShoppingBagIcon/>},
+    {title:"Favorites",icon:<FavoriteIcon/>},
+    {title:"Address",icon:<AddReactionIcon/>},
+    {title:"Payments",icon:<AccountBalanceWalletIcon/>},
+    {title:"Notification",icon:<NotificationsActiveIcon/>},
+    {title:"Events",icon:<EventIcon/>},
+    {title:"Orders",icon:<LogoutIcon/>},
+
 ]
 const ProfileNavigation = () => {
   return (
