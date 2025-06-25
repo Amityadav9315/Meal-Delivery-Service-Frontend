0bf6459a3499e52b9acce4275683493121c9836f
Amit Yadav <amit931552@gmail.com>
2025-06-25 21:11:44 +0530

    fdbjk

--- src/component/Profile/Profile.jsx
+++ src/component/Profile/Profile.jsx
@@ -1,11 +1,12 @@
-import React from 'react'
+import React, { useState } from 'react'
 import ProfileNavigation from './ProfileNavigation'
 
 const Profile = () => {
+    const[ openSideBar,setOpenSideBar]=useState(false);
   return (
     <div className='lg:flex justify-between'>
         <div className='sticky h-[80vh] lg:w-[20%]'>
-           <ProfileNavigation/>
+           <ProfileNavigation open={openSideBar}/>
 
 
         </div>
