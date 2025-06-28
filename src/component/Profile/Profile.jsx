94553f92cd0381fae3c299226b8f148d2b35093d
Amit Yadav <amit931552@gmail.com>
2025-06-28 10:15:52 +0530

    v snl

--- src/component/Profile/Profile.jsx
+++ src/component/Profile/Profile.jsx
@@ -1,42 +1,28 @@
-import React, { useState } from 'react'
-import ProfileNavigation from './ProfileNavigation'
-import { UserProfile } from './UserProfile';
-import Address from './Address';
+import React, { useState } from 'react';
+import ProfileNavigation from './ProfileNavigation';
+import { Route, Routes } from 'react-router-dom';
+import UserProfile from './UserProfile';
 import Orders from './Orders';
-import Favorites from './Favorites';
-import Events from './Events';
+import Adderss from './Adderss';
 
 const Profile = () => {
-    const[ openSideBar,setOpenSideBar]=useState(false);
+  const [openSideBar, setOpenSideBar] = useState(false);
+
   return (
     <div className='lg:flex justify-between'>
-        <div className='sticky h-[80vh] lg:w-[20%]'>
-           <ProfileNavigation open={openSideBar}/>
-
-
-        </div>
-        <div className='lg:w-[80%]'>
-              
-          <Routes>
-            <Route path='/' element={<UserProfile/>}/>
-            <Route path='/orders' element={<Orders/>}/>
-            <Route path='/address' element={<Address/>}/>
-            <Route path='/favorites' element={<Favorites/>}/>
-            <Route path='/events' element={<Events/>}/>
-            
-            
-            
-            
-          </Routes>
-       
-             
-
-
-        </div>
-
-
+      <div className='sticky h-[80vh] lg:w-[20%]'>
+        <ProfileNavigation open={openSideBar} />
+      </div>
+      <div className='lg:w-[80%]'>
+        <Routes>
+          
+          <Route path='/' element={<UserProfile/>} />
+          <Route path='/orders' element={<Orders/>} />
+          <Route path='/address' element={<Adderss/>} />
+        </Routes>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Profile
+export default Profile;
