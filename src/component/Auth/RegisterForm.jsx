3ecf3409c1788c36624fe30461d45bc348e249d7
Amit Yadav <amit931552@gmail.com>
2025-07-05 09:50:27 +0530

    wrnkref

--- src/component/Auth/RegisterForm.jsx
+++ src/component/Auth/RegisterForm.jsx
@@ -4,20 +4,26 @@ import Typography from '@mui/material/Typography';
 import { Field, Form, Formik } from 'formik'
 import React from 'react'
 import { useNavigate } from 'react-router-dom'
+import Select from '@mui/material/Select';
+import MenuItem from '@mui/material/MenuItem';
+import FormControl from '@mui/material/FormControl';
+import InputLabel from '@mui/material/InputLabel';
 const initialValues={
   fullName:"",
   email:"",
   password:"",
   role:"ROLE_CUSTOMER"
+  
 }
 const RegisterForm = () => {
    const navigate=useNavigate()
-  const handleSubmit=()=>{
-  }
+  const handleSubmit=(values)=>{
+    console.log("form values",values)
+  };
   return (
     <div>
       <Typography variant='h5' className='text-center'>
-        Login
+        Register
       </Typography>
       <Formik  onSubmit={handleSubmit} initialValues={initialValues}>
           <Form>
@@ -30,6 +36,15 @@ const RegisterForm = () => {
                                 margin="normal"
                               />
                               <Field
+            
+                              as={TextField}
+                                name="email"
+                                label="email"
+                                fullWidth
+                                variant="outlined"
+                                margin="normal"
+                              />
+                              <Field
                               as={TextField}
                                 name="password"
                                 label="password"
@@ -37,7 +52,23 @@ const RegisterForm = () => {
                                 variant="outlined"
                                 margin="normal"
                               />
-                              <Button sx={{mt:2, padding:"1rem"}} fullWidth type='submit' variant='contained'>Login</Button>
+                    
+  <Field
+  fullWidth
+   margin="normal"
+  as={Select}
+    labelId="demo-simple-select-label"
+    id="role-simple-select"
+    name="Role"
+    
+  
+    // onChange={handleChange}
+  >
+    <MenuItem value={"ROLE_CUSTOMER"}>Customer</MenuItem>
+    <MenuItem value={"ROLE_RESTAURANT-OWNER"}>Restaurant Owner</MenuItem>
+  </Field>
+
+                              <Button sx={{mt:2, padding:"1rem"}} fullWidth type='submit' variant='contained'>Register</Button>
 
 
           </Form>
